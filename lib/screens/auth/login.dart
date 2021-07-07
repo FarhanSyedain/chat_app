d3e7e76861ed3abb370b2848f8a0b461f2b3c713
Farhan Syedain <farhansyedain@gmail.com>
2021-07-07 22:03:43 +0530

    Refactored code a little

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
+import 'components/customAppbar.dart';
 import 'components/loginScreenBody.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -8,31 +8,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(70),
-        child: AppBar(
-          backwardsCompatibility: false,
-          systemOverlayStyle: SystemUiOverlayStyle(
-            statusBarIconBrightness: Brightness.light,
-            statusBarColor: Colors.black,
-          ),
-          backgroundColor: Theme.of(context).backgroundColor,
-          leading: IconButton(
-            padding: EdgeInsets.only(top: 20),
-            icon: Icon(Icons.arrow_back_ios_outlined),
-            onPressed: () {},
-          ),
-          elevation: 0,
-          title: Padding(
-            padding: EdgeInsets.only(top: 20),
-            child: Text(
-              'Log in',
-              style: Theme.of(context).textTheme.headline1,
-            ),
-          ),
-        ),
-      
-      ),
+      appBar: buildAppBar(context, 'Log in'),
       body: LoginScreenBody(),
     );
   }
