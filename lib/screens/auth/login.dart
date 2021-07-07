a605132a6956ece4a0b9b9db50edb4853d277cab
Farhan Syedain <farhansyedain@gmail.com>
2021-07-07 15:01:57 +0530

    Added Login Screen

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+import 'components/loginScreenBody.dart';
 
 class LoginScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      backgroundColor: Theme.of(context).backgroundColor,
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: AppBar(
+          backwardsCompatibility: false,
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarIconBrightness: Brightness.light,
+            statusBarColor: Colors.black,
+          ),
+          backgroundColor: Theme.of(context).backgroundColor,
+          leading: IconButton(
+            padding: EdgeInsets.only(top: 20),
+            icon: Icon(Icons.arrow_back_ios_outlined),
+            onPressed: () {},
+          ),
+          elevation: 0,
+          title: Padding(
+            padding: EdgeInsets.only(top: 20),
+            child: Text(
+              'Log in',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+        ),
       
+      ),
+      body: LoginScreenBody(),
     );
   }
 }
