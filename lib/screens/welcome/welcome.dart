d878f13f73f78ad883be73f815fcfefe315ee255
Farhan Syedain <farhansyedain@gmail.com>
2021-07-14 22:17:19 +0530

    Added one more svg and fixed svg rendering errir'

--- lib/screens/welcome/welcome.dart
+++ lib/screens/welcome/welcome.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/constants.dart';
+import 'package:chat_app/screens/auth/components/customProceedButton.dart';
 import 'package:chat_app/screens/auth/register.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -11,35 +13,51 @@ class WelcomeScreen extends StatefulWidget {
 }
 
 class _WelcomeScreenState extends State<WelcomeScreen> {
+  
   @override
+  final isDarkMode = ThemeMode.system == ThemeMode.dark;
   Widget build(BuildContext context) {
     return Container(
       child: IntroductionScreen(
-        done: Text("Sign up"), // todo: put CustomProceedButton here
+        color: isDarkMode ? kDarkBackgroundColor: kDarkBackgroundColor,
+        done: CustomProceedButton(
+          "Sign up",
+        ), // todo: put CustomProceedButton here
 
         onDone: () {
           Navigator.push(context,
               MaterialPageRoute(builder: (context) => RegisterScreen()));
         },
-        next: Text("Next"), // todo: put CustomProceedButton here
+        // showNextButton: false,
 
+        next: CustomProceedButton("Next"), // todo: put CustomProceedButton here
         pages: [
           PageViewModel(
+            decoration: PageDecoration(
+              imagePadding: EdgeInsets.only(top: 50),
+            ),
             title: "Welcome to Spark",
             body: "A brand new way to connect with people you care about.",
             image: SvgPicture.asset(
-              "vectors/people.svg",
+              "assets/vectors/people.svg",
             ),
           ),
           PageViewModel(
+            decoration: PageDecoration(
+              imagePadding: EdgeInsets.only(top: 50),
+            ),
             title: "Hybrid Voice Texting",
             body:
                 "Make your online communication more personal using voice texting",
             image: SvgPicture.asset(
-              "vectors/voice.svg",
+              "assets/vectors/voice.svg",
             ),
           ),
           PageViewModel(
+            decoration: PageDecoration(
+              imagePadding: EdgeInsets.only(top: 50),
+            ),
+            image: SvgPicture.asset('assets/vectors/getStarted.svg'),
             title: "Get started",
             body: "It's free and easy",
           ),
