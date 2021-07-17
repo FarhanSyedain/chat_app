8dc1e60996393d2d110c24bf66e2b809b9d16a95
Farhan Syedain <farhansyedain@gmail.com>
2021-07-17 22:01:07 +0530

    Added logic for authentication

---
+++ lib/services/auth.dart
@@ -0,0 +1,26 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+
+  Stream<User? > get authStateChanges => _auth.authStateChanges();
+
+  Future<String> registerUser(String email, String password) async {
+    try {
+      _auth.createUserWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      return e.code;
+    }
+    return '';
+  }
+
+  Future<String> loginUser(String email, String password) async {
+    try {
+      _auth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      return e.code;
+    }
+    return '';
+  }
+}
