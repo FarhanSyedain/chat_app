ac831dd92b3833965bf32a8a9512cf57a4f18262
Farhan Syedain <farhansyedain@gmail.com>
2021-07-20 21:00:11 +0530

    Tweaked the code a little so as to return differnt widgets

--- lib/screens/auth/wrapper.dart
+++ lib/screens/auth/wrapper.dart
@@ -1,13 +1,29 @@
+import 'package:chat_app/screens/auth/confirmEmail.dart';
 import 'package:chat_app/screens/welcome/welcome.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key? key}) : super(key: key);
+  Future<void> sign() async {
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     final _user = FirebaseAuth.instance.currentUser;
-    return (_user != null) ? WelcomeScreen() : Container();
+    if (_user == null) {
+      return WelcomeScreen();
+    }
+    final emailVerified = _user.emailVerified;
+    return emailVerified
+        ? Container(
+            child: TextButton(
+                onPressed: () {
+                  sign();
+                },
+                child: Text('Signout')),
+          )
+        : ConfirmEmailScreen();
   }
 }
