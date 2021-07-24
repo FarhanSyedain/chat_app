eeed0b28bd23ce84d3a6e36dd8741a4611f5f9a3
Farhan Syedain <farhansyedain@gmail.com>
2021-07-24 17:31:17 +0530

    Added logic to buttons and added timer for resend email

--- lib/screens/auth/confirmEmail.dart
+++ lib/screens/auth/confirmEmail.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:chat_app/components/customProceedButton.dart';
 import 'package:chat_app/screens/auth/components/customAppbar.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -14,23 +15,35 @@ class ConfirmEmailScreen extends StatefulWidget {
 class _ConfirmEmailScreenState extends State<ConfirmEmailScreen> {
   final _user = FirebaseAuth.instance.currentUser;
   var timer;
+  int timeLeft = 60;
+  int previousTimeLeft = 60;
+  bool sendingEmail = false;
   String? emailAdress = '';
-
+  Timer? timeLeftTimer;
   @override
   void initState() {
-    _user?.sendEmailVerification();
+    _user?.sendEmailVerification().catchError((e) {
+      print(e);
+    });
     timer = Timer.periodic(
       Duration(seconds: 5),
       (_) {
         checkIfVerified();
       },
     );
+    timeLeftTimer = Timer.periodic(
+      Duration(seconds: 1),
+      (timer) {
+        setState(() {
+          timeLeft--;
+          if (timeLeft == 0) {
+            timer.cancel();
+          }
+        });
+      },
+    );
     emailAdress = _user?.email;
     super.initState();
-  }
-
-  Future<void> sign() async {
-    FirebaseAuth.instance.signOut();
   }
 
   Future<void> checkIfVerified() async {
@@ -57,14 +70,38 @@ class _ConfirmEmailScreenState extends State<ConfirmEmailScreen> {
     super.dispose();
   }
 
+  Future<void> resendVerificationEmail() async {
+    if (timeLeft > 0) {
+      return;
+    }
+    await _user?.sendEmailVerification().catchError((e) {
+      //Too many requests sent
+      print(e);
+    });
+    timeLeft = previousTimeLeft * 3;
+    previousTimeLeft = timeLeft;
+    timeLeftTimer = Timer.periodic(
+      Duration(seconds: 1),
+      (timer) {
+        setState(() {
+          timeLeft--;
+          if (timeLeft == 0) {
+            timer.cancel();
+          }
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      appBar: buildAppBar(context, title: 'Verify Email', back: () {
-        FirebaseAuth.instance.signOut();
-        Navigator.pop(context);
-      }),
+      appBar: buildAppBar(
+        context,
+        title: 'Verify Email',
+        showBackButton: false,
+      ),
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 20),
         child: Column(
@@ -100,10 +137,87 @@ class _ConfirmEmailScreenState extends State<ConfirmEmailScreen> {
             SizedBox(
               height: 20,
             ),
-            CustomProceedButton('Resend Email')
+            sendingEmail
+                ? CustomProceedButton('Sending Email.')
+                : GestureDetector(
+                    child: CustomProceedButton(
+                      timeLeft == 0
+                          ? 'Resend Email'
+                          : 'Resend Email (in $timeLeft s)',
+                    ),
+                    onTap: () async {
+                      setState(() {
+                        sendingEmail = true;
+                      });
+                      await resendVerificationEmail();
+                      setState(
+                        () {
+                          sendingEmail = false;
+                        },
+                      );
+                    },
+                  ),
+            SizedBox(
+              height: 13,
+            ),
+            Center(
+              child: Text.rich(
+                TextSpan(
+                  text: 'Clicked the link?  ',
+                  children: [
+                    TextSpan(
+                      text: 'Refresh page',
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () => checkIfVerified(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )
+                  ],
+                  style: TextStyle(
+                    color: Colors.white60,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(child: Container()),
+            Center(
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: 'Sign out',
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () => signOut(),
+                      style: Theme.of(context)
+                          .textTheme
+                          .bodyText1
+                          ?.copyWith(color: Colors.blue),
+                    ),
+                  ],
+                  text: '',
+                ),
+                style: Theme.of(context).textTheme.bodyText2,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
           ],
         ),
       ),
     );
   }
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.pushReplacementNamed(context, '/wrapper');
+  }
+
+  Future<void> refreshPage() async {}
 }
