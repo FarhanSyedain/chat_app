d25ea4ef36beca7ad9c668451f522292485c33b4
Farhan Syedain <farhansyedain@gmail.com>
2021-07-27 11:02:47 +0530

    Redisgned

--- lib/screens/auth/otpScreen.dart
+++ lib/screens/auth/otpScreen.dart
@@ -1,10 +1,8 @@
 import 'package:chat_app/components/customProceedButton.dart';
-import 'package:chat_app/constants.dart';
 import 'package:otp_text_field/otp_field.dart';
 import 'package:otp_text_field/otp_field_style.dart';
 import 'package:otp_text_field/style.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 
 class OTPScreen extends StatefulWidget {
@@ -13,8 +11,6 @@ class OTPScreen extends StatefulWidget {
 }
 
 class _OTPScreenState extends State<OTPScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final TextEditingController _pinPutController = TextEditingController();
   final pinPutDecoration = BoxDecoration(
     color: Colors.grey,
     // borderRadius: BorderRadius.circular(20.0),
@@ -27,74 +23,59 @@ class _OTPScreenState extends State<OTPScreen> {
   );
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: SystemUiOverlayStyle(
-        statusBarColor: kDarkCardColor,
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: kDarkCardColor,
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(40),
-                  bottomLeft: Radius.circular(40),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          SvgPicture.asset(
+            'assets/vectors/phoneAuth.svg',
+            height: MediaQuery.of(context).size.height / 3,
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Verify OTP',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                SizedBox(height: 30),
+                OTPTextField(
+                  length: 6,
+                  width: MediaQuery.of(context).size.width - 40,
+                  fieldWidth: 40,
+                  style: TextStyle(fontSize: 17),
+                  otpFieldStyle: OtpFieldStyle(
+                      borderColor: Colors.grey,
+                      enabledBorderColor: Colors.grey),
+                  textFieldAlignment: MainAxisAlignment.spaceAround,
+                  fieldStyle: FieldStyle.underline,
+                  onCompleted: (pin) {
+                    print("Completed: " + pin);
+                  },
                 ),
-              ),
-              padding: const EdgeInsets.fromLTRB(20, 40, 20, 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SvgPicture.asset(
-                    'assets/vectors/phoneAuth.svg',
-                    height: MediaQuery.of(context).size.height / 3,
+                SizedBox(
+                  height: 40,
+                ),
+                CustomProceedButton('Verify'),
+                Center(
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('Resend Code'),
                   ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Verify OTP',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  SizedBox(height: 30),
-                  OTPTextField(
-                    length: 6,
-                    width: MediaQuery.of(context).size.width - 40,
-                    fieldWidth: 40,
-                    style: TextStyle(fontSize: 17),
-                    otpFieldStyle: OtpFieldStyle(borderColor: Colors.grey,enabledBorderColor: Colors.grey),
-                    textFieldAlignment: MainAxisAlignment.spaceAround,
-                    fieldStyle: FieldStyle.underline,
-                    onCompleted: (pin) {
-                      print("Completed: " + pin);
-                    },
-                  ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  CustomProceedButton('Verify'),
-                  Center(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text('Resend Code'),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/auth/phonenumber.dart
+++ lib/screens/auth/phonenumber.dart
@@ -1,9 +1,6 @@
 import 'package:chat_app/components/customProceedButton.dart';
-import 'package:chat_app/constants.dart';
-import 'package:chat_app/screens/auth/components/customAppbar.dart';
 import 'package:chat_app/screens/auth/components/customTextField.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 
 class PhoneNumberScreen extends StatefulWidget {
@@ -14,50 +11,35 @@ class PhoneNumberScreen extends StatefulWidget {
 class _PhoneNumberScreenState extends State<PhoneNumberScreen> {
   @override
   Widget build(BuildContext context) {
-    return AnnotatedRegion<SystemUiOverlayStyle>(
-      value: SystemUiOverlayStyle(
-        statusBarColor: kDarkCardColor,
-      ),
-      child: Scaffold(
-        backgroundColor: Colors.black,
-        body: Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: kDarkCardColor,
-                borderRadius: BorderRadius.only(
-                  bottomRight: Radius.circular(40),
-                  bottomLeft: Radius.circular(40),
-                ),
-              ),
-              padding: const EdgeInsets.fromLTRB(20, 40, 20, 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SvgPicture.asset(
-                    'assets/vectors/phoneAuth.svg',
-                    height: MediaQuery.of(context).size.height / 3,
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    'Sign In',
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          SizedBox(
+            height: 40,
+          ),
+          SvgPicture.asset(
+            'assets/vectors/phoneAuth.svg',
+            height: MediaQuery.of(context).size.height / 3,
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Sign In',
+            style: TextStyle(
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
             ),
-            SizedBox(
-              height: 50,
-            ),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                children: [
-                  CustomTextField(
+          ),
+          SizedBox(
+            height: 40,
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Form(
+                  child: CustomTextField(
                     'Phone Number',
                     'Enter a valid phone number',
                     (v) {
@@ -69,26 +51,26 @@ class _PhoneNumberScreenState extends State<PhoneNumberScreen> {
                           : 'Please enter a valid phone number';
                     },
                   ),
-                  SizedBox(
-                    height: 30,
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                CustomProceedButton('Get OTP'),
+                SizedBox(
+                  height: 10,
+                ),
+                Center(
+                  child: TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text('Use Email instead?'),
                   ),
-                  CustomProceedButton('Get OTP'),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Center(
-                    child: TextButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      child: Text('Use Email instead?'),
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
