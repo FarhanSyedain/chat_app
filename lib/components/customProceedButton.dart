b7fbbd3846474f68ddf8e68ba33cd6bea46b5870
Farhan Syedain <farhansyedain@gmail.com>
2021-07-27 21:33:29 +0530

    Added OTP screen

--- lib/components/customProceedButton.dart
+++ lib/components/customProceedButton.dart
@@ -3,24 +3,29 @@ import 'package:flutter/material.dart';
 
 class CustomProceedButton extends StatelessWidget {
   final String buttonText;
-
-  CustomProceedButton(this.buttonText);
+  final bool disabled;
+  CustomProceedButton(
+    this.buttonText, {
+    this.disabled = false,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(25),
-        color: kDarkCardColor,
+        color: disabled ? kDarkCardColor.withAlpha(180) : kDarkCardColor,
       ),
       width: double.infinity,
       height: 60,
       alignment: Alignment.center,
       child: Text(
         buttonText,
-        style: Theme.of(context).textTheme.bodyText1?.copyWith(
-              fontSize: 20,
-            ),
+        style: disabled
+            ? Theme.of(context).textTheme.subtitle1?.copyWith(fontSize: 20)
+            : Theme.of(context).textTheme.bodyText1?.copyWith(
+                  fontSize: 20,
+                ),
       ),
     );
   }
--- lib/screens/auth/otpScreen.dart
+++ lib/screens/auth/otpScreen.dart
@@ -1,9 +1,12 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:chat_app/components/customProceedButton.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:otp_text_field/otp_field.dart';
 import 'package:otp_text_field/otp_field_style.dart';
 import 'package:otp_text_field/style.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'dart:async';
 
 class OTPScreen extends StatefulWidget {
   @override
@@ -11,8 +14,60 @@ class OTPScreen extends StatefulWidget {
 }
 
 class _OTPScreenState extends State<OTPScreen> {
+  int timeLeft = 60;
+  int previousTimeLeft = 60;
+  Timer? resendTimer;
+  String? _verificationCode;
+  bool showSpiner = false;
+  bool pinGiven = false;
+  String? pin;
+  @override
+  void initState() {
+    resendTimer = Timer.periodic(
+      Duration(seconds: 1),
+      (timer) {
+        setState(() {
+          timeLeft--;
+        });
+        if (timeLeft == 0) {
+          timer.cancel();
+        }
+      },
+    );
+    _verifyPhoneNumber();
+    super.initState();
+  }
+
+  Future<void> _verifyPhoneNumber() async {
+    await FirebaseAuth.instance.verifyPhoneNumber(
+      phoneNumber: '+911111221122',
+      timeout: const Duration(seconds: 60),
+      verificationCompleted: (PhoneAuthCredential credential) async {},
+      verificationFailed: (FirebaseAuthException e) {
+        AwesomeDialog(
+          context: context,
+          dialogType: DialogType.ERROR,
+          title: 'Error',
+          desc: e.message,
+        ).show();
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        _verificationCode = verificationId;
+
+        AwesomeDialog(
+          context: context,
+          dialogType: DialogType.SUCCES,
+          title: '+911111221122',
+          desc: 'OTP sent successfully.',
+        ).show();
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {},
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    print(FirebaseAuth.instance.currentUser);
     return Scaffold(
       backgroundColor: Colors.black,
       body: Column(
@@ -42,14 +97,15 @@ class _OTPScreenState extends State<OTPScreen> {
                     TextSpan(
                       text: '9596342946',
                       style: Theme.of(context).textTheme.bodyText2,
-                      children: [        
+                      children: [
                         WidgetSpan(
                           child: Padding(
-                            padding: EdgeInsets.only(left:10),
+                            padding: EdgeInsets.only(left: 10),
                             child: Icon(
                               Icons.edit,
                               size: 20,
-                              color: Theme.of(context).textTheme.bodyText1?.color,
+                              color:
+                                  Theme.of(context).textTheme.bodyText1?.color,
                             ),
                           ),
                         ),
@@ -59,34 +115,126 @@ class _OTPScreenState extends State<OTPScreen> {
                 ),
                 SizedBox(height: 30),
                 OTPTextField(
+                  keyboardType: TextInputType.number,
                   length: 6,
                   width: MediaQuery.of(context).size.width - 40,
                   fieldWidth: 40,
                   style: TextStyle(fontSize: 17),
                   otpFieldStyle: OtpFieldStyle(
-                      borderColor: Colors.grey,
-                      enabledBorderColor: Colors.grey),
+                    borderColor: Colors.grey,
+                    enabledBorderColor: Colors.grey,
+                  ),
                   textFieldAlignment: MainAxisAlignment.spaceAround,
                   fieldStyle: FieldStyle.underline,
-                  onCompleted: (pin) {
-                    print("Completed: " + pin);
+                  onChanged: (v) {
+                    print('I am getting called');
+                    print(v.length);
+                    if (v.length == 6) {
+                      setState(
+                        () {
+                          pin = v;
+                          pinGiven = true;
+                        },
+                      );
+                    } else {
+                      setState(
+                        () {
+                          pin = '';
+                          pinGiven = false;
+                        },
+                      );
+                    }
                   },
+                  onCompleted: (v) {},
                 ),
                 SizedBox(
                   height: 40,
                 ),
-                CustomProceedButton('Verify'),
+                GestureDetector(
+                  child: CustomProceedButton(
+                    'Verify',
+                    disabled: !pinGiven,
+                  ),
+                  onTap: pinGiven ? verifyUser : verifyUser,
+                ),
                 Center(
                   child: TextButton(
-                    onPressed: () {},
-                    child: Text('Resend Code'),
+                    onPressed: () {
+                      if (timeLeft == 0)
+                        reSendCode().then(
+                          (value) {
+                            setState(
+                              () {
+                                timeLeft = previousTimeLeft * 2;
+                                previousTimeLeft = timeLeft;
+                              },
+                            );
+                            resendTimer = Timer.periodic(
+                              Duration(seconds: 1),
+                              (timer) {
+                                setState(() {
+                                  timeLeft--;
+                                });
+                                if (timeLeft == 0) {
+                                  timer.cancel();
+                                }
+                              },
+                            );
+                          },
+                        );
+                    },
+                    child: Text(
+                      timeLeft == 0
+                          ? 'Resend Code'
+                          : 'Resend Code ($timeLeft s)',
+                    ),
                   ),
-                )
+                ),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+
+  Future<void> reSendCode() async {}
+
+  Future<void> verifyUser() async {
+    print(pin);
+    FocusScope.of(context).unfocus();
+    print('I am atlest getting exculted');
+    try {
+      setState(() {
+        showSpiner = true;
+      });
+      print('I am atlest getting exculted 2 $pin');
+
+      await FirebaseAuth.instance
+          .signInWithCredential(
+            PhoneAuthProvider.credential(
+              verificationId: _verificationCode!,
+              smsCode: pin!,
+            ),
+          )
+          .then(
+            (value) => print('Successfull'),
+          );
+      print('Hello');
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        '/wrapper',
+        (route) => false,
+      );
+    } catch (e) {
+      print('Threr is an error actually');
+      print(e);
+    }
+
+    setState(
+      () {
+        showSpiner = false;
+      },
     );
   }
 }
