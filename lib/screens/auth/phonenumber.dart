a1ba5e67627041298c93d540c87ab4028ba32990
Farhan Syedain <farhansyedain@gmail.com>
2021-07-28 17:05:20 +0530

    Wrapped inside a SingleChildScroolView

--- lib/screens/auth/phonenumber.dart
+++ lib/screens/auth/phonenumber.dart
@@ -16,80 +16,82 @@ class _PhoneNumberScreenState extends State<PhoneNumberScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          SizedBox(
-            height: 40,
-          ),
-          SvgPicture.asset(
-            'assets/vectors/phoneAuth.svg',
-            height: MediaQuery.of(context).size.height / 3,
-          ),
-          SizedBox(height: 20),
-          Text(
-            'Sign In',
-            style: TextStyle(
-              fontSize: 40,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
             ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Padding(
-            padding: EdgeInsets.all(20),
-            child: Column(
-              children: [
-                Form(
-                  key: _formKey,
-                  child: CustomTextField(
-                    'Phone Number',
-                    'Enter a valid phone number',
-                    (v) {
-                      if (v == null) {
-                        return null;
+            SvgPicture.asset(
+              'assets/vectors/phoneAuth.svg',
+              height: MediaQuery.of(context).size.height / 3,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Sign In',
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                children: [
+                  Form(
+                    key: _formKey,
+                    child: CustomTextField(
+                      'Phone Number',
+                      'Enter a valid phone number',
+                      (v) {
+                        if (v == null) {
+                          return null;
+                        }
+                        return v.length == 10
+                            ? null
+                            : 'Please enter a valid phone number';
+                      },
+                      controller: _controller,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  GestureDetector(
+                    child: CustomProceedButton('Get OTP'),
+                    onTap: () {
+                      if (_formKey.currentState!.validate()) {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return OTPScreen('+91${_controller.text}');
+                            },
+                          ),
+                        );
                       }
-                      return v.length == 10
-                          ? null
-                          : 'Please enter a valid phone number';
                     },
-                    controller: _controller,
                   ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                GestureDetector(
-                  child: CustomProceedButton('Get OTP'),
-                  onTap: () {
-                    if (_formKey.currentState!.validate()) {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return OTPScreen('+91${_controller.text}');
-                          },
-                        ),
-                      );
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Center(
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.pushReplacementNamed(context, '/register');
-                    },
-                    child: Text('Use Email instead?'),
+                  SizedBox(
+                    height: 10,
                   ),
-                ),
-              ],
+                  Center(
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacementNamed(context, '/register');
+                      },
+                      child: Text('Use Email instead?'),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
