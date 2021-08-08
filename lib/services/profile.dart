f6f57c58de7dc8127f582760e8209f476fcfefe9
Farhan Syedain <farhansyedain@gmail.com>
2021-08-08 15:26:09 +0530

    Store profilePicture in deviece

--- lib/services/profile.dart
+++ lib/services/profile.dart
@@ -1,5 +1,7 @@
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:path_provider/path_provider.dart' as pathProvider;
+import 'package:path/path.dart' as path;
 
 class ProfileService {
   static Future<void> removeProfilePicture(String userUid) async {
@@ -17,5 +19,10 @@ class ProfileService {
     final ref =
         FirebaseStorage.instance.ref().child('userProfiles').child(userUid);
     await ref.putFile(profilePicture);
+    final dir = await pathProvider.getApplicationDocumentsDirectory();
+    final storagePath = '$dir/userProfile';
+    await profilePicture.copy(storagePath);
+    //Copied the image to the deviece 
+    
   }
 }
