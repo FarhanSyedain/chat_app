16ba7f25290fb314093b2f1c09e20c78326739d4
Farhan Syedain <farhansyedain@gmail.com>
2021-08-10 16:29:30 +0530

    added boiler plate for emailSentScreen

---
+++ lib/screens/auth/resetEmail.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ResetEmailSendScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
