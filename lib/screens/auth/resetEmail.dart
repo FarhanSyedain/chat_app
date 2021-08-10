452e763861a45b1f2aec89e2dee282bca1f8c886
Farhan Syedain <farhansyedain@gmail.com>
2021-08-10 16:57:38 +0530

    Completed design

--- lib/screens/auth/resetEmail.dart
+++ lib/screens/auth/resetEmail.dart
@@ -1,11 +1,60 @@
+import 'package:chat_app/components/customProceedButton.dart';
+import 'package:chat_app/screens/auth/components/customAppbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class ResetEmailSendScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: buildAppBar(context),
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Email has been sent!',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Text(
+                'Please check your inbox. We\'ve sent you an email to reset your password.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white70,
+                ),
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Center(
+                child: SvgPicture.asset(
+                  'assets/vectors/emailReset.svg',
+                  height: 200,
+                ),
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              GestureDetector(
+                child: CustomProceedButton('Login'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
