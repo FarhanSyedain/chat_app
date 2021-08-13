7cbdc99ef55c4b651ee8e5f24b082d6771ee0a54
Farhan Syedain <farhansyedain@gmail.com>
2021-08-13 20:42:36 +0530

    Refactored

--- lib/screens/auth/login/components/loginScreenBody.dart
+++ lib/screens/auth/login/components/loginScreenBody.dart
@@ -1,7 +1,9 @@
 import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:chat_app/components/customProceedButton.dart';
 import 'package:chat_app/components/dilog/awsomeDilog.dart';
+import 'package:chat_app/screens/auth/components/haveAccount.dart';
 import 'package:chat_app/screens/auth/components/socialAuthRow.dart';
+import 'package:chat_app/screens/auth/login/components/userInputArea.dart';
 import 'package:chat_app/services/auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:loading_overlay/loading_overlay.dart';
@@ -17,11 +19,6 @@ class LoginScreenBody extends StatefulWidget {
 }
 
 class _LoginScreenBodyState extends State<LoginScreenBody> {
-  final _formKey = GlobalKey<FormState>();
-  TextEditingController _emailControler = TextEditingController();
-  TextEditingController _passwordControler = TextEditingController();
-  bool userNotFound = false;
-  bool incorrectPassword = false;
   bool loading = false;
 
   void changeVal(v) {
@@ -48,71 +45,12 @@ class _LoginScreenBodyState extends State<LoginScreenBody> {
               SizedBox(height: 20),
               SocialMediaRowWithPhoneNumberSwitch(changeVal),
               SizedBox(height: 5),
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    Column(
-                      children: [
-                        CustomTextField(
-                          'Email',
-                          'Enter your email',
-                          emailValidator,
-                          controller: _emailControler,
-                          errorMessage: userNotFound
-                              ? 'User with this email doesn\'t exist'
-                              : null,
-                        ),
-                        SizedBox(height: 20),
-                        CustomTextField(
-                          'Password',
-                          'Enter your password',
-                          null,
-                          errorMessage: incorrectPassword
-                              ? 'Incorrect password or no password set'
-                              : null,
-                          isPassword: true,
-                          controller: _passwordControler,
-                        ),
-                      ],
-                    ),
-                    SizedBox(height: 5),
-                    Padding(
-                      padding: EdgeInsets.only(left: 10, top: 5),
-                      child: Text.rich(
-                        TextSpan(
-                          text: 'Forgot Password?',
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = forgotPassword,
-                        ),
-                        style: Theme.of(context).textTheme.subtitle2,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(height: 25),
-              GestureDetector(
-                child: CustomProceedButton('Log In'),
-                onTap: () => login(context),
-              ), // Login Button Here
+              UserInputArea(changeVal),
               SizedBox(height: 10),
-              Center(
-                child: Text.rich(
-                  TextSpan(
-                    children: [
-                      TextSpan(
-                        text: 'Sign up',
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () => Navigator.of(context)
-                              .pushReplacementNamed('/register'),
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ],
-                    text: 'Don\'t have an account?   ',
-                  ),
-                  style: Theme.of(context).textTheme.bodyText2,
-                ),
+              HaveOrHaveNotAnAccount(
+                pageName: 'Sign Up',
+                title: 'Don\'t have an account?  ',
+                route: '/register',
               ),
             ],
           ),
@@ -120,69 +58,4 @@ class _LoginScreenBodyState extends State<LoginScreenBody> {
       ),
     );
   }
-
-  void forgotPassword() {
-    Navigator.pushNamed(context, '/forgotPassword');
-  }
-
-  Future<void> login(BuildContext context) async {
-    setState(() {
-      userNotFound = false;
-      incorrectPassword = false;
-      loading = true;
-    });
-    FirebaseAuthException? response =
-        await context.read<AuthService>().loginUser(
-              _emailControler.text,
-              _passwordControler.text,
-            );
-    setState(() {
-      loading = false;
-    });
-    if (response!.code == '') {
-      Navigator.pushNamedAndRemoveUntil(
-        context,
-        '/wrapper',
-        (r) => false,
-      );
-    } else {
-      print(response);
-      switch (response.code) {
-        case 'user-not-found':
-          {
-            setState(() {
-              userNotFound = true;
-            });
-            break;
-          }
-        case 'wrong-password':
-          {
-            print(response.code);
-            setState(() {
-              incorrectPassword = true;
-            });
-            break;
-          }
-        default:
-          {
-            showAwsomeDilog(
-              DialogType.ERROR,
-              'Something unexpected occured',
-              'An unexpected error occured. Please try again.',
-              context,
-            );
-          }
-      }
-    }
-  }
-}
-
-String? emailValidator(value) {
-  if (value == null) {
-    return 'Enter an email';
-  }
-  if (!validateEmail(value)) {
-    return 'Enter a valid email adress';
-  }
-  return null;
 }
