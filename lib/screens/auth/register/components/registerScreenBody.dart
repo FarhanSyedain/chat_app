41e0d275c3bc12e4ef3771b0d85c977c3ead467a
Farhan Syedain <farhansyedain@gmail.com>
2021-08-13 22:46:56 +0530

    Move regex based files to sub folder

--- lib/screens/auth/register/components/registerScreenBody.dart
+++ lib/screens/auth/register/components/registerScreenBody.dart
@@ -1,7 +1,7 @@
 import 'package:chat_app/screens/auth/components/socialAuthRow.dart';
 import 'package:chat_app/services/auth.dart';
-import 'package:chat_app/utilities/emailRegexValidator.dart';
-import 'package:chat_app/utilities/passwordValidator.dart';
+import 'package:chat_app/utilities/regex/emailRegexValidator.dart';
+import 'package:chat_app/utilities/regex/passwordValidator.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:chat_app/components/customProceedButton.dart';
