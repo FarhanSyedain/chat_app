0b35356498cd8f044be88050fd7a997c43c65cc5
Farhan Syedain <farhansyedain@gmail.com>
2021-08-13 23:14:33 +0530

    Move inside a completly different file

--- lib/screens/auth/phoneAuth/phonenumber.dart
+++ lib/screens/auth/phoneAuth/phonenumber.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/screens/auth/phoneAuth/components/phoneNumberInput.dart';
+
 import '/components/customProceedButton.dart';
 import '/screens/auth/constants.dart';
 import '/screens/auth/phoneAuth/otpScreen.dart';
@@ -82,42 +84,3 @@ class _PhoneNumberScreenState extends State<PhoneNumberScreen> {
   }
 }
 
-class PhoneNumberInput extends StatelessWidget {
-  final number;
-  final changeVal;
-  PhoneNumberInput(this.number, this.changeVal);
-
-  @override
-  Widget build(BuildContext context) {
-    return InternationalPhoneNumberInput(
-      initialValue: number,
-      countrySelectorScrollControlled: false,
-      scrollPadding: EdgeInsets.all(0),
-      selectorTextStyle: Theme.of(context).textTheme.bodyText2,
-      autoValidateMode: AutovalidateMode.always,
-      searchBoxDecoration: InputDecoration(
-        enabledBorder: unFocusedBorder(context),
-        focusedBorder: focusedBorder(context),
-      ),
-      selectorConfig: SelectorConfig(
-        leadingPadding: 0,
-        selectorType: PhoneInputSelectorType.DIALOG,
-        useEmoji: true,
-        setSelectorButtonAsPrefixIcon: true,
-        trailingSpace: false,
-      ),
-      textStyle: Theme.of(context).textTheme.bodyText1,
-      spaceBetweenSelectorAndTextField: 5,
-      ignoreBlank: true,
-      onInputChanged: (phoneNumber) {
-        changeVal(phoneNumber);
-      },
-      inputDecoration: InputDecoration(
-        hintText: 'Phone Number',
-        hintStyle: Theme.of(context).textTheme.bodyText2,
-        focusedBorder: focusedBorder(context),
-        enabledBorder: unFocusedBorder(context),
-      ),
-    );
-  }
-}
