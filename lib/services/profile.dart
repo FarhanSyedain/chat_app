5408567c8c8d62599f5e377626db6ba381cbae0f
Farhan Syedain <farhansyedain@gmail.com>
2021-08-16 20:31:25 +0530

    Added func getProfile

--- lib/services/profile.dart
+++ lib/services/profile.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path_provider/path_provider.dart' as pathProvider;
@@ -48,12 +49,28 @@ class ProfileService {
     final downloadUrl = await ref.getDownloadURL();
     final response = await http.get(Uri(path: downloadUrl));
     final tempDir = await pathProvider.getTemporaryDirectory();
-    final dir = await pathProvider.getApplicationDocumentsDirectory();
     //?Temp file for to avoid creating empty files
     File file = File('${tempDir.path}/profilePicture');
     file.writeAsBytesSync(response.bodyBytes);
 
     await file.copy('${dir.path}/profilePicture');
     await file.delete();
+  }
+
+  Future<void> getProfile(FirebaseAuth _auth) async {
+    final prefs = await SharedPreferences.getInstance();
+    final uid = _auth.currentUser!.uid;
+    final userInfo =
+        await FirebaseFirestore.instance.collection('users').doc(uid).get();
+    if (userInfo.exists) {
+      final data = userInfo.data();
+      for (var key in data!.keys) {
+        prefs.setString(key, data[key]);
+      }
+      await prefs.setBool('profileSet', true);
+    } else {
+      await prefs.setBool('profileSet', false);
+    }
+    await updateProfilePicture();
   }
 }
