5e78e913b1b49f0b53cd5731e6b68942ff540b95
Farhan Syedain <farhansyedain@gmail.com>
2021-08-24 20:46:44 +0530

    Fized profile fetch  bug

--- lib/services/profile.dart
+++ lib/services/profile.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path_provider/path_provider.dart' as pathProvider;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
 
 class ProfileService {
   static Future<void> removeProfilePicture(String userUid) async {
@@ -21,40 +20,34 @@ class ProfileService {
       File profilePicture, String userUid) async {
     final prefs = await SharedPreferences.getInstance();
     try {
-      prefs.setBool('DPUpdateFinished', false);
       final ref =
           FirebaseStorage.instance.ref().child('userProfiles').child(userUid);
       await ref.putFile(profilePicture);
       final dir = await pathProvider.getApplicationDocumentsDirectory();
       final storagePath = '${dir.path}/userProfile';
       await profilePicture.copy(storagePath);
-      //Copied the image to the deviece
-      prefs.setBool('DPUpdateFinished', true);
     } catch (e) {
       print('This is the eroor $e');
     }
   }
 
-  static Future<void> updateProfilePicture({skipCheck = true}) async {
+  static Future<File?> updateProfilePicture({skipCheck = true}) async {
     final dir = await pathProvider.getApplicationDocumentsDirectory();
     if (!skipCheck) {
       //Check if profilePicture already exists
       if (File('${dir.path}/profilePicture').existsSync()) {
-        return;
+        return null;
       }
     }
     final userUid = FirebaseAuth.instance.currentUser!.uid;
     final ref =
-        FirebaseStorage.instance.ref().child('userProfiles').child(userUid);
-    final downloadUrl = await ref.getDownloadURL();
-    final response = await http.get(Uri(path: downloadUrl));
+        FirebaseStorage.instance.ref().child('userProfiles').child(userUid);    
     final tempDir = await pathProvider.getTemporaryDirectory();
-    //?Temp file for to avoid creating empty files
     File file = File('${tempDir.path}/profilePicture');
-    file.writeAsBytesSync(response.bodyBytes);
-
+    await ref.writeToFile(file);
     await file.copy('${dir.path}/profilePicture');
-    await file.delete();
+    file.delete();
+    return File('${tempDir.path}/profilePicture');
   }
 
   Future<void> getProfile(FirebaseAuth _auth) async {
@@ -67,6 +60,7 @@ class ProfileService {
       for (var key in data!.keys) {
         prefs.setString(key, data[key]);
       }
+  
       await prefs.setBool('profileSet', true);
     } else {
       await prefs.setBool('profileSet', false);
