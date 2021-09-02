fe5879cb982edeb55ce1f2d2340d33fcdb42d14c
Farhan Syedain <farhansyedain@gmail.com>
2021-09-02 09:08:31 +0530

    Added more parimeters

--- lib/screens/auth/components/customAppbar.dart
+++ lib/screens/auth/components/customAppbar.dart
@@ -6,31 +6,41 @@ PreferredSize buildAppBar(
   String? title,
   void Function()? back,
   showBackButton = true,
+  paddingTop = 20.0,
+  bgColor,
+  height,
+  leading,
+  leadingWidth,
+List<Widget>? actions 
 }) =>
     PreferredSize(
-      preferredSize: Size.fromHeight(70),
+      preferredSize: Size.fromHeight(height ?? 70.0),
       child: AppBar(
+      
         backwardsCompatibility: false,
         systemOverlayStyle: SystemUiOverlayStyle(
           statusBarIconBrightness: Brightness.light,
-          statusBarColor: Colors.black,
+          statusBarColor: Theme.of(context).backgroundColor,
         ),
-        backgroundColor: Theme.of(context).backgroundColor,
+        backgroundColor: bgColor ?? Theme.of(context).backgroundColor,
         leading: showBackButton
             ? IconButton(
-                padding: EdgeInsets.only(top: 20),
+                padding: EdgeInsets.only(top: paddingTop),
                 icon: Icon(
                   Icons.arrow_back_ios_outlined,
                   color: Theme.of(context).iconTheme.color,
                 ),
                 onPressed: back,
               )
-            : null,
+            :  leading,
+          
+            actions:actions,
         // centerTitle: showBackButton ? false : true,
         elevation: 0,
+        
         title: title != null
             ? Padding(
-                padding: EdgeInsets.only(top: 20),
+                padding: EdgeInsets.only(top: paddingTop),
                 child: Text(
                   title,
                   style: Theme.of(context).textTheme.headline1,
