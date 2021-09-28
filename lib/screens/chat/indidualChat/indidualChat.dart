870bafa1260689ca9ab7c579fcd82fe23ab06d99
= <farhansyedain@gmail.com>
2021-09-28 21:30:30 +0530

    Cleaning

--- lib/screens/chat/indidualChat/indidualChat.dart
+++ lib/screens/chat/indidualChat/indidualChat.dart
@@ -1,4 +1,5 @@
 import 'package:chat_app/screens/auth/components/customAppbar.dart';
+import 'package:chat_app/screens/chat/indidualChat/IndidualChatBody.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
 
@@ -7,46 +8,52 @@ class IndidualChat extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      appBar: buildAppBar(context,
-          height: 65.0,
-          showBackButton: false,
-          titleWidget: Row(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              backButton(context, () {}, 0.0),
-              SizedBox(
-                width: 5,
-              ),
-              Container(
-                height: 35,
-                width: 35,
-                decoration:
-                    BoxDecoration(borderRadius: BorderRadius.circular(100)),
-                clipBehavior: Clip.hardEdge,
-                child: Image.asset('assets/dummy/profilePicture.jpg'),
-              ),
-              SizedBox(width: 8),
-              Text(
-                'Linus Torvalds',
-                style: Theme.of(context).textTheme.headline2,
-              )
-            ],
+      appBar: appBar(context),
+      body: IndidualChatBody(),
+    );
+  }
+
+  PreferredSize appBar(context) {
+    return buildAppBar(
+      context,
+      height: 65.0,
+      showBackButton: false,
+      titleWidget: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          backButton(context, () {}, 0.0),
+          SizedBox(
+            width: 5,
           ),
-          bgColor: Theme.of(context).backgroundColor,
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.video_call),
-              padding: EdgeInsets.zero,
-            ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.call,
-              ),
-              padding: EdgeInsets.zero,
-            ),
-          ]),
+          Container(
+            height: 35,
+            width: 35,
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(100)),
+            clipBehavior: Clip.hardEdge,
+            child: Image.asset('assets/dummy/profilePicture.jpg'),
+          ),
+          SizedBox(width: 8),
+          Text(
+            'Linus Torvalds',
+            style: Theme.of(context).textTheme.headline2,
+          )
+        ],
+      ),
+      bgColor: Theme.of(context).backgroundColor,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.video_call),
+          padding: EdgeInsets.zero,
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.call,
+          ),
+          padding: EdgeInsets.zero,
+        ),
+      ],
     );
   }
 }
