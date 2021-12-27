0d4b10631439cbe3d4ce87cc32f41a0717b8c91b
= <farhansyedain@gmail.com>
2021-12-27 22:45:39 +0530

    Removed socail auth n replace w/ chatchy txt inste ad

--- lib/screens/auth/components/socialAuthRow.dart
+++ lib/screens/auth/components/socialAuthRow.dart
@@ -1,48 +1,77 @@
 import 'package:flutter/material.dart';
 import '../components/socialMediaLoginButton.dart';
-import '../../../utilities/auth.dart';
 
 class SocialMediaRowWithPhoneNumberSwitch extends StatelessWidget {
   final Function(dynamic) changeVal;
-  SocialMediaRowWithPhoneNumberSwitch(this.changeVal);
+  final String uniqueText;
+  SocialMediaRowWithPhoneNumberSwitch(
+    this.changeVal,
+    this.uniqueText,
+  );
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        SizedBox(height: 50),
+        SizedBox(height: 30),
         Text(
-          'Sign in with one of these options.',
-          style: Theme.of(context).textTheme.bodyText2,
-        ),
-        SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Expanded(
-              child: GestureDetector(
-                onTap: () => signInWithGoogle(context, changeVal),
-                child: SocialMediaLoginButton('assets/vectors/google_icon.svg'),
-              ),
-            ),
-            SizedBox(width:25),
-            Expanded(
-              child: GestureDetector(
-                onTap: () => signWithTwitter(context, changeVal),
-                child: SocialMediaLoginButton('assets/vectors/twitter_icon.svg'),
-              ),
-            ),
-          ],
-        ),
-        SizedBox(height: 10),
-        Center(
-          child: TextButton(
-            child: Text('Use phone number instead.'),
-            onPressed: () {
-              Navigator.of(context).pushReplacementNamed('/phoneAuth');
-            },
+          'Chat Freely!',
+          style: TextStyle(
+            fontFamily: 'MontserratB',
+            fontSize: 50,
+            color: Colors.white,
           ),
         ),
+        Text(
+          'Welcome,',
+          style: TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 30,
+          ),
+        ),
+        Text(
+          'Sign $uniqueText below',
+          style: TextStyle(
+            fontFamily: 'Montserrat',
+            fontSize: 30,
+          ),
+        ),
+        SizedBox(
+          height: 40,
+        ),
+        // Text(
+        //   'Sign in with one of these options.',
+        //   style: Theme.of(context).textTheme.bodyText2,
+        // ),
+        // SizedBox(height: 20),
+        // SocialMediaLoginButton('Sign in with Phone Instead'),
+        // Row(
+        //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //   children: [
+        //     Expanded(
+        //       child: GestureDetector(
+        //         onTap: () => signInWithGoogle(context, changeVal),
+        //         child: SocialMediaLoginButton('assets/vectors/google_icon.svg'),
+        //       ),
+        //     ),
+        //     SizedBox(width:25),
+        //     Expanded(
+        //       child: GestureDetector(
+        //         onTap: () => signWithTwitter(context, changeVal),
+        //         child: SocialMediaLoginButton('assets/vectors/twitter_icon.svg'),
+        //       ),
+        //     ),
+        //   ],
+        // ),
+        // SizedBox(height: 10),
+        // Center(
+        //   child: TextButton(
+        //     child: Text('Use phone number instead.'),
+        //     onPressed: () {
+        //       Navigator.of(context).pushReplacementNamed('/phoneAuth');
+        //     },
+        //   ),
+        // ),
       ],
     );
   }
