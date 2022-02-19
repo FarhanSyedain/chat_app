2660178a072d7ac43502e05bbc823161b42b36b1
farhansyedain <farhansyedain@gmail.com>
2022-02-19 18:46:10 +0530

    wrap with background thing

--- lib/screens/chat/indidualChat/IndidualChatBody.dart
+++ lib/screens/chat/indidualChat/IndidualChatBody.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/components/background.dart';
 import 'package:chat_app/models/chat/chat.dart';
 import 'package:chat_app/models/extras/enums.dart';
 import 'package:chat_app/models/inputBar.dart';
@@ -35,100 +36,105 @@ class _BodyState extends State<Body> {
   Widget build(BuildContext context) {
     final InputBarProvider inputBarProvider =
         Provider.of<InputBarProvider>(context);
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            reverse: true,
-            controller: controller,
-            // shrinkWrap: true,
-            itemCount: widget.provider.messages.length + 1,
-            itemBuilder: (context, index) {
-              if (index == widget.provider.messages.length) {
-                return ChatStartingWidget();
-              }
-              return ChangeNotifierProvider.value(
-                value: widget.provider.messages.elementAt(index),
-                builder: (context, child) {
-                  return SwipeTo(
-                    child: MessageBubble(index - 1),
-                    onLeftSwipe: () {
-                      inputBarProvider.replyToMessage(
-                        widget.provider.messages.elementAt(index),
-                      );
-                    },
-                    onRightSwipe: () {
-                      inputBarProvider.replyToMessage(
-                        widget.provider.messages.elementAt(index),
-                      );
-                    },
-                  );
-                },
-              );
-            },
+    return BackgroundWrapper(
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              reverse: true,
+              controller: controller,
+              // shrinkWrap: true,
+              itemCount: widget.provider.messages.length + 1,
+              itemBuilder: (context, index) {
+                if (index == widget.provider.messages.length) {
+                  return ChatStartingWidget();
+                }
+                return ChangeNotifierProvider.value(
+                  value: widget.provider.messages.elementAt(index),
+                  builder: (context, child) {
+                    return SwipeTo(
+                      child: MessageBubble(index - 1),
+                      onLeftSwipe: () {
+                        inputBarProvider.replyToMessage(
+                          widget.provider.messages.elementAt(index),
+                        );
+                      },
+                      onRightSwipe: () {
+                        inputBarProvider.replyToMessage(
+                          widget.provider.messages.elementAt(index),
+                        );
+                      },
+                    );
+                  },
+                );
+              },
+            ),
           ),
-        ),
-        SizedBox(
-          height: 5,
-        ),
-        if (inputBarProvider.message != null)
-          Container(
-            height: 60,
-            margin: EdgeInsets.only(left: 10, right: 10),
-            width: double.infinity,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(25),
-                topRight: Radius.circular(25),
+          SizedBox(
+            height: 5,
+          ),
+          if (inputBarProvider.message != null)
+            Container(
+              height: 60,
+              margin: EdgeInsets.only(left: 10, right: 10),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+                color: Colors.black45,
               ),
-              color: Colors.black45,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  height: 35,
-                  padding: const EdgeInsets.only(left: 15.0, top: 10),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        inputBarProvider.message!.sender == Sender.me
-                            ? 'You'
-                            : widget.provider.title,
-                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                              color: Theme.of(context).colorScheme.secondary,
-                            ),
-                      ),
-                      IconButton(
-                        padding: EdgeInsets.zero,
-                        onPressed: () => inputBarProvider.resetReplyTo(),
-                        icon: Icon(
-                          Icons.cancel_outlined,
-                          size: 15,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: 35,
+                    padding: const EdgeInsets.only(left: 15.0, top: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          inputBarProvider.message!.sender == Sender.me
+                              ? 'You'
+                              : widget.provider.title,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(
+                                color: Theme.of(context).colorScheme.secondary,
+                              ),
                         ),
-                      ),
-                    ],
+                        IconButton(
+                          padding: EdgeInsets.zero,
+                          onPressed: () => inputBarProvider.resetReplyTo(),
+                          icon: Icon(
+                            Icons.cancel_outlined,
+                            size: 15,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Divider(
-                  color: Colors.white,
-                  height: 8,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 15.0),
-                  child: Text(
-                    inputBarProvider.message!.data!,
+                  Divider(
+                    color: Colors.white,
+                    height: 8,
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: Text(
+                      inputBarProvider.message!.data!,
+                    ),
+                  ),
+                ],
+              ),
             ),
+          InputBar(
+            provider: widget.provider,
+            controller: controller,
           ),
-        InputBar(
-          provider: widget.provider,
-          controller: controller,
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
